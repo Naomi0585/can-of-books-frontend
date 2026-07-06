53b909594a8165319e0f29ecf5b111bae77a1e64
Naomi Torres <ivanys.ntm@gmail.com>
2026-07-06 14:50:39 -0700

    starter code

---
+++ src/About.jsx
@@ -0,0 +1,18 @@
+
+import { Component } from 'react';
+
+class About extends Component {
+
+  render() {
+  
+    return (
+      <>
+        <h1>About the Devs</h1>
+        <h3>Code Fellows Student</h3>
+        <p>Code Fellows Student is a full stack software developer proficient in many programming languages including Markdown, HTML, CSS, JavaScript, Python, Java and C#.  See more on Github!</p>
+      </>
+    );
+  }
+}
+
+export default About;
